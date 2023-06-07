e7997643bf44166d77ffb6b86e2f49721d04ac04
walidulhasan <walidulhasanboniamin@gmail.com>
2023-06-07 15:20:12 +0600

    bfb-Angular first Component

---
+++ src/app/AngularForBeginners/components/angular-first-components/angular-first-components.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-first-components',
+  templateUrl: './angular-first-components.component.html',
+  styleUrls: ['./angular-first-components.component.scss']
+})
+export class AngularFirstComponentsComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InitialComponent } from './AngularForBeginners/components/initial/initial.component';
+import { AngularFirstComponentsComponent } from './AngularForBeginners/components/angular-first-components/angular-first-components.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InitialComponent
+    InitialComponent,
+    AngularFirstComponentsComponent
   ],
   imports: [
     BrowserModule,
