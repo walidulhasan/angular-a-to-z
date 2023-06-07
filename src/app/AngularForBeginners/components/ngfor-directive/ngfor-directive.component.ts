a7aa69007fb28306a64e753eb41830183a046971
walidulhasan <walidulhasanboniamin@gmail.com>
2023-06-07 20:03:39 +0600

    ngStyle

--- src/app/AngularForBeginners/components/ngfor-directive/ngfor-directive.component.ts
+++ src/app/AngularForBeginners/components/ngfor-directive/ngfor-directive.component.ts
@@ -5,13 +5,25 @@ import { COURSES } from '../../db-data';
 @Component({
   selector: 'app-ngfor-directive',
   templateUrl: './ngfor-directive.component.html',
-  styleUrls: ['./ngfor-directive.component.scss']
+  styleUrls: ['./ngfor-directive.component.scss'],
 })
 export class NgforDirectiveComponent {
-  data:Array<Courses>=COURSES
-  isCategoryAdvanced=()=>{
-    return{
-      'advanced':this.data[2].category=='ADVANCED'
+  data: Array<Courses> = COURSES;
+  isCategoryAdvanced = () => {
+    return {
+      advanced: this.data[2].category == 'ADVANCED',
+    };
+  };
+
+  isDescription = () => {
+    if (this.data[2].category == 'ADVANCED') {
+      return {
+        'background-color': 'yellow',
+      };
+    } else {
+      return {
+        'background-color': 'yellow',
+      };
     }
-  }
+  };
 }
